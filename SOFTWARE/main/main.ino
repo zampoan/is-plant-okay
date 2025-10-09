b1145aba6ad3267fd696bcded96777db4052f7be
Zheng Yao Siah <zhengyaosiah@hotmail.com>
2025-10-09 11:23:01 +1100

    feat: added pins for mcu

--- SOFTWARE/main/main.ino
+++ SOFTWARE/main/main.ino
@@ -2,7 +2,6 @@
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1351.h>
 #include <Arduino_HS300x.h>
-#include <Wire.h>
 
 // OLED dimensions
 #define SCREEN_WIDTH 128
@@ -41,47 +40,16 @@ void setup() {
   Serial.println("Serial ready.");
   Wire.begin();
 
-  // if (!Wire.begin()){
-  //   Serial.println("Failed to initalize I2C");
-  // }
-
   if(!HS300x.begin()){
     Serial.println("Failed to initialize HS300x sensor");
     while(1);
   }
 
-  // if(display.begin()){
-  //   Serial.println("Failed to initialize OLED");
-  // }
   display.begin();
-  //oled_setup();
+
   Serial.println("All initialized...");
 }
 
 void loop() {
-  float temperature = HS300x.readTemperature();
-  float humidity = HS300x.readHumidity();
-  Serial.print("Temperature: ");
-  Serial.println(temperature);
-  Serial.print("Humidity: ");
-  Serial.println(humidity);
-
-  if (abs(old_temperature - temperature) >= 0.5 || abs(old_humidity - humidity) >= 1 )
-  {
-  // print each of the sensor values
-  Serial.print("Temperature = ");
-  Serial.print(temperature);
-  Serial.println(" °C");
-
-  Serial.print("Humidity    = ");
-  Serial.print(humidity);
-  Serial.println(" %");
-
-  // print an empty line
-  Serial.println();
-
-  // wait 1 second to print again
-  delay(1000);
-   }
-
+  
 }
